4501387b9db25627cfb5afdfa350c05cd5e8b7a4
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-08 12:47:03 +0100

    add save

--- app/services/start_scrap.rb
+++ app/services/start_scrap.rb
@@ -3,20 +3,31 @@ require 'open-uri'
 
 class StartScrap
 
+  def initialize
+  	@url = "https://coinmarketcap.com/all/views/all/"
+  end
+
   def get_cryptos
-    array = []
-    webpage = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    @all_cryptos = []
+    webpage = Nokogiri::HTML(open(@url))
     webpage.css('tbody>tr').each do |row|
       hash = {}
       hash['Currency'] = row.css('.currency-name')[0]['data-sort']
       hash['Price'] = row.css('.text-right')[1]['data-sort']
-      array << hash
+      @all_cryptos << hash
     end
-    array
+    @all_cryptos
+  end
+
+  def save
+  	@all_cryptos.each do |i|
+  		Crypto.create!(name: i['Currency'], value: i['Price'])
+  	end
   end
 
   def perform
-    puts get_cryptos
+    get_cryptos
+    save
   end
 
 end
