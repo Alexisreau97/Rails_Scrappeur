e55ead969f48fd5fd83ef4d8e06094647e5f3c73
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-08 11:24:32 +0100

    add scrappeur

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+
+gem 'nokogiri', '~> 1.8', '>= 1.8.5'
+gem 'open_uri_redirections', '~> 0.2.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use postgresql as the database for Active Record
--- app/services/start_scrap.rb
+++ app/services/start_scrap.rb
@@ -1,15 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+
 class StartScrap
 
-  def initialize
-	
-  end
-
-  def save
-	
+  def get_cryptos
+    array = []
+    webpage = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    webpage.css('tbody>tr').each do |row|
+      hash = {}
+      hash['Currency'] = row.css('.currency-name')[0]['data-sort']
+      hash['Price'] = row.css('.text-right')[1]['data-sort']
+      array << hash
+    end
+    array
   end
 
   def perform
-	
+    puts get_cryptos
   end
-  
+
 end
